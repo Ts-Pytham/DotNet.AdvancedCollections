53e4e30bb92c5755f983fe5df78e95011e64e3fc
Johan Sánchez <johansanchezal@unimagdalena.edu.co>
2026-02-09 12:49:44 -0500

    SortedList migration to NET8

---
+++ DotNet.AdvancedCollections/List/SortedList/Criterion.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNet.AdvancedCollections.List.SortedList;
+
+/// <summary>
+/// Represents an enum for the sort criterion.
+/// </summary>
+public enum Criterion
+{
+    /// <summary>
+    /// Sort the elements in ascending order.
+    /// </summary>
+    Ascending,
+
+    /// <summary>
+    /// Sort the elements in descending order.
+    /// </summary>
+    Descending,
+}
