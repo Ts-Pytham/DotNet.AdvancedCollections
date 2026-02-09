085380a8746abf58a51813ecb5be6ba1cb92b4cf
Johan Sánchez <johansanchezal@unimagdalena.edu.co>
2026-02-09 16:29:38 -0500

    BinarySearchTree class migration to NET 8

---
+++ DotNet.AdvancedCollections/Tree/BinarySearchTree/IBinarySearchTree.cs
@@ -0,0 +1,19 @@
+﻿namespace DotNet.AdvancedCollections.Tree.BinarySearchTree;
+
+/// <summary>
+/// Represents BinarySearchTree Interface.
+/// </summary>
+public interface IBinarySearchTree<T> : ICollection<T>
+{
+    /// <summary>
+    /// Traversal type.
+    /// InOrder, PostOrder and PreOrder.
+    /// </summary>
+    TraversalType Traversal { get; set; }
+
+    /// <summary>
+    /// Check if the tree is empty.
+    /// </summary>
+    /// <returns>If the tree is empty, returns true, otherwise returns false.</returns>
+    bool IsEmpty();
+}
